cf122fed324957c3db3adef3d1b294f9d3aa3144
sano <guram.sanikidze02@geolab.edu.ge>
2018-05-19 13:04:02 +0400

    added drop down menu

--- src/components/pages/BudgetItem/index.js
+++ src/components/pages/BudgetItem/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // my comp
 import Breadcrumbs from '../../moleculas/Breadcrumbs';
@@ -21,34 +21,73 @@ const productImg = {
     backgroundImage: `url(${product_image})`
 }
 
-const BudgetItem = () => {
-    return (
-        <div id="budget_item">
-            <Breadcrumbs />
+class BudgetItem extends Component {
 
-            <div id="container" style={containerStyle}>
-                <div id="cover" style={productImg}></div>
-                <div id="edit_and_friends">
-                    <div id="friends_and_add_more_friends">
-                        <FriendList id="friend_list"/>
-                        <MainIcons src={add_gray_icon} id="add_friends"/>
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+          showMenu: false,
+        }
+        
+        this.showMenu = this.showMenu.bind(this);
+    }
+
+    showMenu(){
+        this.setState({
+            showMenu: !this.state.showMenu
+        })
+    }
+
+    render(){
+
+        const ulStyle = {
+            background: COLORS.white,
+            boxShadow: MAIN_SHADOW,
+            border: `1pt solid ${COLORS.light_gray}`
+        }
+
+        return (
+            <div id="budget_item">
+                <Breadcrumbs />
+
+                <div id="container" style={containerStyle}>
+                    <div id="cover" style={productImg}></div>
+                    <div id="edit_and_friends">
+                        <div id="friends_and_add_more_friends">
+                            <FriendList id="friend_list"/>
+                            <MainIcons src={add_gray_icon} id="add_friends"/>
+                        </div>
+                        <MainIcons src={bullets_gray_icon} onClick={this.showMenu} id="edit_budget"/>
                     </div>
-                    <MainIcons src={bullets_gray_icon} id="edit_budget"/>
-                </div>
-                <Title id="title">Title</Title>
-                <MainLine />
-                <NumberInput/>
-                <div>
-                    {ProgressBar(70)}
-                    <LightText id="light_text">105$ of 150$. Needed: 45$</LightText>
-                </div>
-                <div id="more_info">
-                    <FriendList showFullName={true} />
-                </div>
+                    {
+                            this.state.showMenu ? (
+                                <div className="drop_down_menu">  
+                                    <ul style={ulStyle}>
+                                        <LightText>
+                                            <li>Edit</li>
+                                            <li>Delete</li>
+                                            <li>Report</li>
+                                        </LightText>
+                                    </ul>
+                                </div>
+                                ) : null
+                    }
+                    <Title id="title">Title</Title>
+                    <MainLine />
+                    <NumberInput/>
+                    <div>
+                        {ProgressBar(70)}
+                        <LightText id="light_text">105$ of 150$. Needed: 45$</LightText>
+                    </div>
+                    <div id="more_info">
+                        <FriendList showFullName={true} />
+                    </div>
 
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default BudgetItem;
